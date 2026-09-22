7979dda0549a8cfd6759db697f2bd18e27a36e03
CyzmiX <itzmedigamingx@gmail.com>
2026-09-22 08:03:04 +0000

    tables / rows

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, vec};
 
 #[derive(PartialEq, PartialOrd, Debug)]
 enum Value {
@@ -18,56 +18,103 @@ enum Operator {
     NotEq,
 }
 
-impl Operator {
-    fn compare(&self, a: &Value, b: &Value) -> bool {
-        match self {
-            Operator::Bigger => a > b,
-            Operator::Smaller => a < b,
-            Operator::BiggerOrEq => a >= b,
-            Operator::SmallerOrEq => a <= b,
-            Operator::Eq => a == b,
-            Operator::NotEq => a != b,
-        }
-    }
+// impl Operator {
+//     fn compare(&self, a: &HashMap<String, Value>, b: &HashMap<&String, &Value>) -> bool {
+//         match self {
+//             Operator::Bigger => a > b,
+//             Operator::Smaller => a < b,
+//             Operator::BiggerOrEq => a >= b,
+//             Operator::SmallerOrEq => a <= b,
+//             Operator::Eq => a == b,
+//             Operator::NotEq => a != b,
+//         }
+//     }
+// }
+
+#[derive(Debug, PartialEq)]
+struct Table {
+    rows: Vec<HashMap<String, Value>>,
+    name: String,
 }
 
 #[derive(Debug)]
 struct DB {
-    data: HashMap<String, Value>,
+    //data: HashMap<String, Value>,
+    tables: HashMap<String, Table>,
 }
 
 impl DB {
-    fn set(&mut self, name: &str, data: Value) {
-        self.data.insert(String::from(name), data);
+    pub fn new() -> Self {
+        DB {
+            tables: HashMap::new(),
+        }
     }
 
-    fn get(&self, name: &str) -> Option<&Value> {
-        self.data.get(name)
+    fn new_table(&mut self, name: &str, rows: Vec<(String, Value)>) -> Result<bool, bool> {
+        let t = Table {
+            rows: vec![HashMap::from_iter(rows)],
+            name: name.to_string(),
+        };
+        if self.tables.contains_key(name) {
+            return Err(false);
+        }
+        return Ok(None == self.tables.insert(String::from(name), t));
     }
 
-    fn get_if<F>(&self, filter_method: F) -> HashMap<&String, &Value>
-    where
-        F: Fn(&Value) -> bool,
-    {
-        self.data.iter().filter(|f| filter_method(f.1)).collect()
-    }
-
-    fn get_with_op(&self, op: Operator, value: Value) -> HashMap<&String, &Value> {
-        self.data
-            .iter()
-            .filter(|f| op.compare(f.1, &value))
-            .collect()
+    fn from(&mut self, name: &str) -> Option<&mut Table> {
+        self.tables.get_mut(name)
     }
 }
 
+impl Table {
+    fn add(&mut self, data: Vec<(String, Value)>) {
+        self.rows.push(HashMap::from_iter(data));
+    }
+
+    fn get(&mut self, value: Vec<(String, Value)>) -> Option<&mut HashMap<String, Value>> {
+        let target = HashMap::from_iter(value);
+
+        self.rows.iter_mut().find(|row| **row == target)
+    }
+
+    fn get_if<F>(&self, filter_method: F) -> Vec<&HashMap<String, Value>>
+    where
+        F: Fn(&HashMap<String, Value>) -> bool,
+    {
+        self.rows.iter().filter(|f| filter_method(*f)).collect()
+    }
+
+    // fn get_if<F>(&self, filter_method: F) -> HashMap<&String, &Value>
+    // where
+    //     F: Fn(&HashMap<String, Value>) -> bool,
+    // {
+    //     self.rows.iter().filter(|f| filter_method(f.1)).collect()
+    // }
+
+    // fn get_with_op(&self, op: Operator, value: Value) -> HashMap<&String, &Value> {
+    //     self.rows
+    //         .iter()
+    //         .filter(|f| op.compare(f.1, &value))
+    //         .collect()
+    // }
+}
+
 fn main() {
-    let mut mydb = DB {
-        data: HashMap::new(),
-    };
+    let mut mydb = DB::new();
 
-    mydb.set("hello", Value::Bool(true));
-    mydb.set("hi", Value::String(String::from("20")));
-    mydb.set("nihai", Value::Int(5));
+    match mydb.new_table("players", vec![("hp".to_string(), Value::Int(0))]) {
+        Ok(_) => println!("{:?}", "created table succesfully"),
+        Err(_) => println!("Oh no"),
+    }
 
-    println!("{:?}", mydb.get_if(|e| e.eq(&Value::Bool(true))).keys())
+    mydb.from("players")
+        .unwrap()
+        .add(vec![("hp".to_string(), Value::Int(100))]);
+
+    println!(
+        "{:?}",
+        mydb.from("players")
+            .unwrap()
+            .get_if(|item| item["hp"] > Value::Int(10))
+    );
 }
